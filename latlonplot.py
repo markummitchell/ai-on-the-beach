57a41d9c57a0ec240b8cde28cc08f9ab3e7b9433
markummitchell <markummitchell@gmail.com>
2019-01-25 19:47:27 -0800

    British Oceanographic Data Centre

--- latlonplot.py
+++ latlonplot.py
@@ -19,7 +19,7 @@ with open ('Beneath The Waves - Blue Shark Atlantic - Data Jan 21, 2019.csv', 'r
             lons.append (float (lonStr))
             msgDates.append (datetime.strptime (datStr, '%m/%d/%y %H:%M'))
 
-# https://matplotlib.org/basemap/users/examples.html
+# Based on https://matplotlib.org/basemap/users/examples.html
 
 map = Basemap (projection = 'ortho', lat_0 = 42, lon_0 = -69.4, resolution = 'l')
 map.drawcoastlines (linewidth = 0.25)
@@ -29,5 +29,6 @@ map.drawmapboundary (fill_color = 'aqua')
 map.drawmeridians (np.arange (0, 360, 30))
 map.drawparallels (np.arange (-90, 90, 30))
 # compute native map projection coordinates of lat/lon grid
-map.plot (lons, lats, linewidth = 0.5, color = 'r')
+map.plot (lons, lats, linewidth = 2.5, color = 'r', latlon = True)
+plt.title ("{} through {}" . format (msgDates [0], msgDates [-1]))
 plt.show()
