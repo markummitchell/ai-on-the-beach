8b215fd7eac65204fa344b45805f8588a622121c
markummitchell <markummitchell@gmail.com>
2019-02-05 01:02:01 -0800

    Cleaned up. Shark movement shown versus time

--- maps.ngdcc.noaa.gov/bathysphere.py
+++ maps.ngdcc.noaa.gov/bathysphere.py
@@ -3,13 +3,19 @@
 import csv
 from datetime import datetime
 from mpl_toolkits.basemap import Basemap, cm
+import math
 import matplotlib.pyplot as plt
 from matplotlib.patches import Circle
 import numpy as np
 from scipy.io import netcdf
+from time import sleep
+
+# Some settings
+is3d = True
 
 FILENAME = 'etopo1_bedrock.nc'
 LINECOLOR = 'lime' # https://matplotlib.org/examples/color/named_colors.html
+PAUSESECONDS = 0.00001 # Nonzero value seems to be required
 
 # Based on https://matplotlib.org/basemap/users/examples.html. The parameter
 # mmap=False prevents issues related to closing plots/files when arrays are
@@ -51,7 +57,6 @@ plt.ylabel ('latitude')
 # GEBCO data is based on SRTM30 according to GEBCO_2014.html,
 # but Basemap seems to be based on WGS84 according to https://media.readthedocs.org/pdf/basemaptutorial/latest/basemaptutorial.pdf
 
-is3d = False
 if is3d:
     map = Basemap (projection = 'stere', lon_0 = lonmin, lat_0 = 90, lat_ts = latmin, \
                    llcrnrlat=latmin, urcrnrlat=latmax, \
@@ -76,45 +81,40 @@ map.drawmapboundary (fill_color = 'aqua')
 map.drawmeridians (np.arange (0, 360, 1))
 map.drawparallels (np.arange (-90, 90, 1))
 
-# convert netcdf arrays to regular array. Actually, we will generate
-# the lon and lat 2d arrays using map.makegrid - so there is an implicit
-# assumption that the lon and lat arrays coming from netcdf are evenly
-# spaced from the lon/lat lower limits to the lon/lat upper limits, respectively
+# limits are -5443 to 1298 (elemin to elemax)
+contours = np.array([-5500., -5000., -4500., -4000., -3500., -3000., -2500., -2000., -1500., -1000., -500., 0., 500., 1000., 1500.])
+
+# Convert netcdf arrays to regular array. Actually, we will generate
+# the lon and lat 2d arrays using map.makegrid. Note that there is a subtle
+# nonlinearity in the latitude versus row number - as seen in lat.csv file below
 ele = np.zeros ((elecdf.shape[0], elecdf.shape[1]))
 lon = np.zeros ((elecdf.shape[0], elecdf.shape[1]))
 lat = np.zeros ((elecdf.shape[0], elecdf.shape[1]))
 for i in range (ele.shape[0]):
     for j in range (ele.shape[1]):
-        ele [i] [j] = elecdf [i] [j]
+        ele [i] [j] = elecdf [j] [i]
         lon [i] [j] = loncdf [i]
         lat [i] [j] = latcdf [j]
-
-# dumping data as csv files
-print ("Writing contours.csv")
-with open ("ele.csv", "w") as f:
-    for i in range (ele.shape[0]):
-        for j in range (ele.shape[1]):
-            f.write ("({}, {}, {})\n" . format (lon [i] [j], lat [i] [j], ele [i] [j]))
-        
-# limits are -5443 to 1298 (elemin to elemax)
-contours = np.array([-5500., -5000., -4500., -4000., -3500., -3000., -2500., -2000., -1500., -1000., -500., 0., 500., 1000., 1500.])
-
-lon, lat = map.makegrid (elecdf.shape[0], elecdf.shape[1])
-lonmin = np.min (lon)
-lonmax = np.max (lon)
-latmin = np.min (lat)
-latmax = np.max (lat)
-print ("grid limits: " + str (lonmin) + "<lon<" + str (lonmax) + " " + str (latmin) + "<lat<" + str (latmax))
 x, y = map (lon, lat)
 cs = map.contourf (x, y, ele, contours, cmap = cm.s3pcpn)
 
-# color scale
+# Color scale
 cbar = map.colorbar (cs,location = 'right', pad = '5%')
 cbar.set_label ('Elevation (meters)')
 
-# waypoints
-lats = []
-lons = []
+# Show circle at northernmost point in nova scotia which is northwest of Meat Point, according
+# to https://stackoverflow.com/questions/49134634/how-to-draw-circle-in-basemap-or-add-artiste
+#circle = Circle (xy = map (-60.593352, 47.041354), radius = (map.ymax - map.ymin) / 60, fill = False)
+#plt.gca().add_patch (circle)
+
+# Labels
+plt.xlabel ('longitude')
+plt.ylabel ('latitude')
+plt.title (FILENAME)
+
+# Waypoints
+lonLast = None
+latLast = None
 with open ('../Beneath The Waves - Blue Shark Atlantic - Data Jan 21, 2019.csv', 'r') as f:
     reader = csv.DictReader (f)
     headers = reader.fieldnames
@@ -122,31 +122,13 @@ with open ('../Beneath The Waves - Blue Shark Atlantic - Data Jan 21, 2019.csv',
         latStr = line ['Latitude']
         lonStr = line ['Longitude']
         if latStr != '' and lonStr != '':
-            lats.append (float (latStr))
-            lons.append (float (lonStr))
-map.plot (lons, lats, linewidth = 2.5, color = LINECOLOR, latlon = True)
-
-# show circle at northernmost point in nova scotia which is northwest of Meat Point, according
-# to https://stackoverflow.com/questions/49134634/how-to-draw-circle-in-basemap-or-add-artiste
-circle = Circle (xy = map (-60.593352, 47.041354), radius = (map.ymax - map.ymin) / 60, fill = False)
-plt.gca().add_patch (circle)
-
-# show map with bathysphere data
-plt.xlabel ('longitude')
-plt.ylabel ('latitude')
-plt.title (FILENAME)
-plt.show()
-
-# bathysphere-only contours
-plt.close()
-plt.contour (lon, lat, ele, contours)
-for lonx in range (-70, -55 + 1, 1):
-    plt.gca().add_line (plt.Line2D ([lonx, lonx], [35, 50], color='r', linewidth=0.5))
-for laty in range (35, 50 + 1, 1):
-    plt.gca().add_line (plt.Line2D ([-70, -55], [laty, laty], color='r', linewidth=0.5))
-plt.show()
-
-# longitude for point northwest of Meat Point
-lonslice = elecdf.data [:, 565]
-plt.plot (lonslice)
-plt.show ()
+            lon = float (lonStr)
+            lat = float (latStr)
+            if lonLast != None:
+                lons = [lonLast, lon]
+                lats = [latLast, lat]
+                map.plot (lons, lats, linewidth = 1.2, color = LINECOLOR, latlon = True)
+            lonLast = lon
+            latLast = lat
+            # Besides the delay, this also seems trigger the display, and without it nothing appears
+            plt.pause (PAUSESECONDS)
